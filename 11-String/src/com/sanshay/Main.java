f0e741a7b2c0763be2882141389f07629c64d86f
Sanshay Patiyal <sanshaypatiyal406@gmail.com>
2026-07-11 17:59:00 +0530

    some

--- 11-String/src/com/sanshay/Main.java
+++ 11-String/src/com/sanshay/Main.java
@@ -1,17 +1,34 @@
 package com.sanshay;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        String name = "Sanshay Patiyal";
-        System.out.println(name);
+//        String name = "Sanshay Patiyal";
+//        System.out.println(name);
+//
+//
+//        String a = "Sanshay";
+//        System.out.println(a);
+//        a = "Patiyal";
+//        System.out.println(a);
+        Scanner sc = new Scanner(System.in);
+        String find = sc.nextLine();
 
 
-        String a = "Sanshay";
-        System.out.println(a);
-        a = "Patiyal";
-        System.out.println(a);
+        int len = find.length();
+
+        for (int i = 0; i < len / 2; i++) {
+            if (find.charAt(i) != find.charAt(len - i - 1)) {
+                System.out.println("not aplaidnromw");
+                return;
+
+            }
+
+            System.out.println("pal");
+
+        }
 
     }
-
 }
